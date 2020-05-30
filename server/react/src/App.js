83aea1f7394dbd2248e5bbdac46a9899d9c6e805
dnorkett <dnorkett@gmail.com>
2020-05-30 01:53:09 -0500

    updated react handleChange functions to bring it in line with current standards

--- server/react/src/App.js
+++ server/react/src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    let char = document.getElementById('searchbar').value;
+    console.log(char);
+    console.log(e)
+    
+    
+
+/*     fetch('/api/characters/' + char)
+        .then(data => data.json())
+        .then(json => {
+            this.setState({
+                characters: json
+            });                        
+        }); */
+  }
+
+  render() {
+    let characters = this.state.characters;
+    
+     characters = characters.map((character, index) => {
+      
+        return (
+            <li key={index}>
+                <span className="name">{character.obj.name}</span>
+                <span className="species">{character.obj.species}</span>
+                <span className="home">{character.obj.homePlanet}</span>
+                <span className="force">{character.obj.forceSensitive}</span>
+                <span className="quote">{character.obj.notableQuote}</span>
+            </li>            
+        );
+    }); 
+
+    return (
+        <div id="char-container">
+            <form id="search" onSubmit={this.handleChange}>
+                <label>Enter character name:</label>
+                <input type="text" placeholder="character" id="searchbar" required />
+                <input type="submit" value="Find character" />
+            </form>
+            <ul>{characters}</ul>
+        </div>
+    );
+  }
 }
+
 
 export default App;
